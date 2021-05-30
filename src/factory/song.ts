a861be3ed12243e9ea19258a3425b309631b936c
ehsueh <emma.yc.hsueh@gmail.com>
2021-05-30 09:15:39 -0400

    fix bugs

--- src/factory/song.ts
+++ src/factory/song.ts
@@ -4,6 +4,7 @@ import {
   json,
   Result,
   Bytes,
+  log,
 } from "@graphprotocol/graph-ts";
 
 import { Song } from "../../generated/schema"
@@ -22,10 +23,10 @@ export function loadOrCreateSong(
 }
 
 export function fillMetaDataFromIPFS(song: Song): void {
-
-  let ipfsParts: string[] = song.url.split("/")
+  let ipfsParts: string[] = song.metadataUrl.split("/")
   let ipfsHash: string = ipfsParts[ipfsParts.length - 1]
   if (ipfsParts.length > 0) {
+    log.warning("fill {}", [ipfsHash])
     let data = ipfs.cat("/ipfs/" + ipfsHash)
     if (data === null) {
       // Retry might fix it
@@ -39,6 +40,7 @@ export function fillMetaDataFromIPFS(song: Song): void {
         let jsonData = result.value;
         let jsonObject = jsonData.toObject();
         song.title = jsonObject.get("name").toString();
+        log.warning("fill {}", [song.title])
         song.description = jsonObject.get("description").toString()
         if (jsonObject.get("image").isNull() === false) {
           song.image = jsonObject.get("image").toString()
--- src/mapping/song.ts
+++ src/mapping/song.ts
@@ -1,7 +1,8 @@
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, log } from "@graphprotocol/graph-ts"
 import {
   Approval,
   ApprovalForAll,
+  EthunesSongs,
   Transfer
 } from "../../generated/EthunesSongs/EthunesSongs"
 import { ZERO_ADDRESS } from "../constants"
@@ -13,18 +14,22 @@ export function handleApproval(event: Approval): void {}
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleTransfer(event: Transfer): void {
+
   let from = loadOrCreateAccount(event.params.from.toHexString())
   let to = loadOrCreateAccount(event.params.to.toHexString())
   let tokenId = event.params.tokenId
-
+  
   let song = loadOrCreateSong(tokenId.toString())
   song.owner = to.id
   
   if (from.id == ZERO_ADDRESS) {
+    let contract = EthunesSongs.bind(event.address)
     // Genesis
     song.creator = to.id
+    song.metadataUrl = contract.tokenURI(tokenId)
     fillMetaDataFromIPFS(song)
   }
+
   from.save()
   to.save()
   song.save()
